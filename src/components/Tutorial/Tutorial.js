d58ec023e6c497fe1ba3a29836093917ce4cc8ce
kamikuroro <p.xywang@gmail.com>
2019-06-23 15:13:49 -0400

    testing pushing to dev master

--- src/components/Tutorial/Tutorial.js
+++ src/components/Tutorial/Tutorial.js
@@ -6,7 +6,7 @@ class Tutorial extends Component{
     render(){
         return(
             <div className="get-compelet-plan">
-                    <h2 className="complete-plan-title">Easy to Use, easy to browse</h2>
+                    <h2 className="complete-plan-title">Easy to Use, easy to browse (Tutorial)</h2>
             </div>
         
          
