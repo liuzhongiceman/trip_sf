feacf2be50fef48515b8bf08939e98d010fdd975
kamikuroro <p.xywang@gmail.com>
2019-06-26 05:28:20 -0400

    on page /plan, sidemenu onSelect will now re-render the context area on its rightside

---
+++ src/pages/Plan/Calendar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export class Calendar extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+        
+    }
+
+    render(){
+        return(
+            <div>
+                <h3>I am Calendar</h3>
+            </div>
+        )
+    }
+}
---
+++ src/pages/Plan/Map.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export class Map extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+        
+    }
+
+    render(){
+        return(
+            <div>
+                <h3>I am Map</h3>
+            </div>
+        )
+    }
+}
